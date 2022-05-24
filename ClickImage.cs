2db1aab59b94036f372398cb68855e73104b759a
Hazmat11 <ebertrand@gaming.tech>
2022-05-24 12:00:26 +0200

    First Commit

---
+++ ClickImage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickImage : MonoBehaviour
+{
+    public void Red()
+    {
+        Debug.Log("RED!");
+    }
+
+    public void Blue()
+    {
+        Debug.Log("BLUE!");
+    }
+
+    public void Purple()
+    {
+        Debug.Log("PURPLE!");
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
