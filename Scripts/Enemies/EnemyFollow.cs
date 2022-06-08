34b6c4c92c039ec99c098f54ab64106a4671768c
Hazmat11 <ebertrand@gaming.tech>
2022-06-08 15:48:42 +0200

    push before merge

--- Scripts/Enemies/EnemyFollow.cs
+++ Scripts/Enemies/EnemyFollow.cs
@@ -15,7 +15,7 @@ public class EnemyFollow : MonoBehaviour
     {
         player = GameObject.Find("MainChar");
         Vector3 pos = transform.position;
-        pos.z = 1;
+        pos.z = 0;
         transform.position = pos;
         rb = this.GetComponent<Rigidbody2D>();
     }
